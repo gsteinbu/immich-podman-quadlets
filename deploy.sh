4187cb076f551f54f3c4e2518ae30f648e0e6620
Georg Steinbuss <gitlab@ech.me>
2026-07-08 19:27:37 +0200

    add password name to prompt

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ if podman secret exists immich_db_password; then
     echo "Secret 'immich_db_password' already exists."
 else
     echo "Secret not found. Please enter the password for immich_db_password:"
-    systemd-ask-password | podman secret create immich_db_password -
+    systemd-ask-password "immich_db_password" | podman secret create immich_db_password -
 fi
 
 export DB_DATA_LOCATION="$HOME/immich/db"
